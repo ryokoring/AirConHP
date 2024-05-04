d433be11bd81e3545c4882a6b403bbae051084f7
ryokoring <ryojikitamaru@gmail.com>
2024-05-05 04:28:19 +0900

    add div

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -8,14 +8,26 @@ export default function Page() {
           <option value="男">男</option>
           <option value="女">女</option>
           </select>
+          <div>
           <label htmlFor="name">お名前・法人名</label>
           <input id="name" name="name" />
+          </div>
+
+          <div>
           <label htmlFor="email">email</label>
           <input id="email" name="email" type="email" />
+          </div>
+
+          <div>
           <label htmlFor="phone">電話番号</label>
           <input id="phone" name="phone" type="tel"></input>
+          </div>
+
+          <div>
           <label htmlFor="message">Message</label>
           <textarea id="message" name="message"></textarea>
+          </div>
+          
           <button type="submit">Submit</button>
         </form>
       </>
