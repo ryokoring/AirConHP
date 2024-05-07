9213d1574a1f38a10f52e888a4f2854ede7b86bc
ryokoring <ryojikitamaru@gmail.com>
2024-05-07 16:08:44 +0900

    change design

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,59 +1,90 @@
-import Image from 'next/image'
-import Link from 'next/link'
+'use client'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 import { getApp } from '@/lib/newt'
 import styles from './styles.module.css'
 
-export async function Header() {
-  const app = await getApp()
+export function Header() {
+  const [app, setApp] = useState(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    const fetchApp = async () => {
+      const appData: any = await getApp()
+      setApp(appData)
+    }
+    fetchApp()
+  }, [])
 
   return (
     <>
       <header className={styles.Header}>
         <div className={styles.Header_Inner}>
-          <Link className={styles.Title} href="/">
-            {/*アイコンの話
-           {app.icon?.type === 'emoji' && (
-            <span className={styles.Title_Icon}>{app.icon.value}</span>
-          )}
-          {app.icon?.type === 'image' && (
-            <span className={styles.Title_Icon}>
-              <Image src={app.icon.value} alt="" width="26" height="26" />
-            </span>
-          )} */}
+          <div className={styles.Title} onClick={() => router.push('/')}>
             <div className={styles.Title_Text}>Logo</div>
-          </Link>
+          </div>
           <div className={styles.Link}>999-8888-7777</div>
         </div>
       </header>
       <header className={styles.SubHeader}>
         <div className={styles.SubHeaderText}>
           <div>
-            <Link href="/pages/page-1">
-              <span className={styles.SubHeaderMargin}>ホーム</span>
-            </Link>
-            <Link href="/ConAirCon">
-              <span className={styles.SubHeaderMargin}>個人用エアコン</span>
-            </Link>
-            <Link href="/CorpAirCon">
-              <span className={styles.SubHeaderMargin}>法人用エアコン</span>
-            </Link>
-            <Link href="/pages/page-2">
-              <span className={styles.SubHeaderMargin}>できることリスト</span>
-            </Link>
-            <Link href="/Contact">
-              <span className={styles.SubHeaderMargin}>お問い合わせ</span>
-            </Link>
-            <Link href="/pages/page-4">
-              <span className={styles.SubHeaderMargin}>企業情報</span>
-            </Link>
-            <Link href="/pages/page-5">
-              <span className={styles.SubHeaderMargin}>採用情報</span>
-            </Link>
-            <Link href="/pages/page-6">
-              <span className={styles.SubHeaderMargin}>
-                プライバシーポリシー
-              </span>
-            </Link>
+            <span
+              style={{ cursor: 'pointer' }}
+              className={styles.SubHeaderMargin}
+              onClick={() => router.push('/pages/page-1')}
+            >
+              ホーム
+            </span>
+            <span
+              style={{ cursor: 'pointer' }}
+              className={styles.SubHeaderMargin}
+              onClick={() => router.push('/ConAirCon')}
+            >
+              個人用エアコン
+            </span>
+            <span
+              style={{ cursor: 'pointer' }}
+              className={styles.SubHeaderMargin}
+              onClick={() => router.push('/CorpAirCon')}
+            >
+              法人用エアコン
+            </span>
+            <span
+              style={{ cursor: 'pointer' }}
+              className={styles.SubHeaderMargin}
+              onClick={() => router.push('/pages/page-2')}
+            >
+              できることリスト
+            </span>
+            <span
+              style={{ cursor: 'pointer' }}
+              className={styles.SubHeaderMargin}
+              onClick={() => router.push('/Contact')}
+            >
+              お問い合わせ
+            </span>
+            <span
+              style={{ cursor: 'pointer' }}
+              className={styles.SubHeaderMargin}
+              onClick={() => router.push('/pages/page-4')}
+            >
+              企業情報
+            </span>
+            <span
+              style={{ cursor: 'pointer' }}
+              className={styles.SubHeaderMargin}
+              onClick={() => router.push('/pages/page-5')}
+            >
+              採用情報
+            </span>
+            <span
+              style={{ cursor: 'pointer' }}
+              className={styles.SubHeaderMargin}
+              onClick={() => router.push('/pages/page-6')}
+            >
+              プライバシーポリシー
+            </span>
           </div>
         </div>
       </header>
